14750d9a690c217c3eaa72705d17dea9c1124540
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-29 19:30:21 +0200

    Add missing exception import

--- source/src/main/java/fci/swe2/onlineshopapi/Account.java
+++ source/src/main/java/fci/swe2/onlineshopapi/Account.java
@@ -1,5 +1,7 @@
 package fci.swe2.onlineshopapi;
 
+import fci.swe2.onlineshopapi.exceptions.ValidationException;
+
 abstract class Account {
     protected long userID;
     protected String username;

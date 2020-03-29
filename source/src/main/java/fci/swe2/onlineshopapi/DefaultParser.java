e7af5e54cec7b546f650dd2b486274aa1f5f8533
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-29 16:17:42 +0200

    Change parseBody function to JSONObject

--- source/src/main/java/fci/swe2/onlineshopapi/DefaultParser.java
+++ source/src/main/java/fci/swe2/onlineshopapi/DefaultParser.java
@@ -1,13 +1,14 @@
 package fci.swe2.onlineshopapi;
 
 import com.sun.net.httpserver.HttpExchange;
+import org.json.JSONObject;
 
 public class DefaultParser implements HTTPExchangeParser {
     private HttpExchange exchange;
 
     DefaultParser(HttpExchange exchange){}
 
-    public String getURLpath(){}
+    public String[] getURLpath(){}
     public String getParameter(String method, String key){}
-    public String parseBody(){}
+    public JSONObject parseBody(){}
 }

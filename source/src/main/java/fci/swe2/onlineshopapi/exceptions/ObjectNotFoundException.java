f14345701477c07db9ac5d352472af3712819e51
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-29 18:57:58 +0200

    Custom exceptions needed in registration

---
+++ source/src/main/java/fci/swe2/onlineshopapi/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,5 @@
+package fci.swe2.onlineshopapi.exceptions;
+
+class ObjectNotFoundException extends ValidationException{
+    private static final long serialVersionUID = 7840844788649672210L;
+};
---
+++ source/src/main/java/fci/swe2/onlineshopapi/exceptions/ValidationException.java
@@ -0,0 +1,5 @@
+package fci.swe2.onlineshopapi.exceptions;
+
+class ValidationException extends Exception{
+    private static final long serialVersionUID = 7840844788649672208L;
+};

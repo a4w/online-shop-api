800c9199fa81f75b097aa98d460677b350af39a3
ahmeddrawy <ahmedhanfy1111@gmail.com>
2020-03-29 19:54:56 +0200

    make the exceptions classes public

--- source/src/main/java/fci/swe2/onlineshopapi/exceptions/EmailAlreadyExistsException.java
+++ source/src/main/java/fci/swe2/onlineshopapi/exceptions/EmailAlreadyExistsException.java
@@ -1,5 +1,5 @@
 package fci.swe2.onlineshopapi.exceptions;
 
-class EmailAlreadyExistsException extends ValidationException{
+public class EmailAlreadyExistsException extends ValidationException{
     private static final long serialVersionUID = 7840844788649672212L;
 };

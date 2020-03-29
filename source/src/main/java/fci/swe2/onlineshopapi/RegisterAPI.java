50a76a033b38bb5b7eb2e5b8d942ff2999f6f2b9
ahmeddrawy <ahmedhanfy1111@gmail.com>
2020-03-29 19:02:22 +0200

    handle Json object and register various accounts

--- source/src/main/java/fci/swe2/onlineshopapi/RegisterAPI.java
+++ source/src/main/java/fci/swe2/onlineshopapi/RegisterAPI.java
@@ -2,16 +2,19 @@ package fci.swe2.onlineshopapi;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class RegisterAPI implements HttpHandler {
     HTTPExchangeParser myParser = null;
+    JSONObject jsonObject = null ;
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         myParser = new DefaultParser(exchange);
         String []urlParameters=  myParser.getURLpath();
+        jsonObject = myParser.parseBody();
         if(urlParameters.length <2){
             /// todo error
             return;
@@ -33,10 +36,41 @@ public class RegisterAPI implements HttpHandler {
         }
     }
 
-    public void registerCustomer(HttpExchange exchange){
-
+    private void registerCustomer(HttpExchange exchange){
+        Customer customer = getCustomerFromJSON();
+        customer.register();
+        //todo handle the exceptions in setting the attributes
     }
-    public void registerStoreOwner(HttpExchange exchange){}
-    public void registerAdmin(HttpExchange exchange){}
+    private void registerStoreOwner(HttpExchange exchange){
+        StoreOwner storeOwner = getStoreOwnerFromJSON();
+        storeOwner.register();
+        //todo handle the exceptions in setting the attributes
+    }
+    private void registerAdmin(HttpExchange exchange){
+        Admin admin = getAdminFromJSON();
+        admin.register();
+        //todo handle the exceptions in setting the attributes
+    }
+    private Customer getCustomerFromJSON(){
+        String email= jsonObject.get("email").toString();
+        String username= jsonObject.get("username").toString();
+        String password= jsonObject.get("email").toString();
+        long userID = jsonObject.getLong("userID");
+        return new Customer(userID,username , email,password) ;
+    }
+    private Admin getAdminFromJSON(){
+        String email= jsonObject.get("email").toString();
+        String username= jsonObject.get("username").toString();
+        String password= jsonObject.get("email").toString();
+        long userID = jsonObject.getLong("userID");
+        return new Admin(userID,username , email,password) ;
+    }
+    private StoreOwner getStoreOwnerFromJSON(){
+        String email= jsonObject.get("email").toString();
+        String username= jsonObject.get("username").toString();
+        String password= jsonObject.get("email").toString();
+        long userID = jsonObject.getLong("userID");
+        return new StoreOwner(userID,username , email,password) ;
+    }
 
 }

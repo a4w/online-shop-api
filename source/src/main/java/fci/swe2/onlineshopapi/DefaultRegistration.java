aea0e9b8514d84e296f6bde47817548a25a6abd0
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-29 19:43:40 +0200

    Implement DefaultRegistration

--- source/src/main/java/fci/swe2/onlineshopapi/DefaultRegistration.java
+++ source/src/main/java/fci/swe2/onlineshopapi/DefaultRegistration.java
@@ -3,5 +3,8 @@ package fci.swe2.onlineshopapi;
 import fci.swe2.onlineshopapi.exceptions.ValidationException;
 
 public class DefaultRegistration implements RegistrationBehaviour{
-    public <T> void register(T obj) throws ValidationException{}
+    public <T> void register(T obj) throws ValidationException{
+        Repository<T> repository = RepoFactory.getMapper(obj);
+        repository.store(obj);
+    }
 }

8a1d96f6958edac1b5a2ef65f06b8241830e2388
ahmeddrawy <ahmedhanfy1111@gmail.com>
2020-03-29 18:23:02 +0200

    handle the various case in handle in registerApi

--- source/src/main/java/fci/swe2/onlineshopapi/RegisterAPI.java
+++ source/src/main/java/fci/swe2/onlineshopapi/RegisterAPI.java
@@ -4,14 +4,39 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class RegisterAPI implements HttpHandler {
-    public void registerCustomer(HttpExchange exchange){}
+    HTTPExchangeParser myParser = null;
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        myParser = new DefaultParser(exchange);
+        String []urlParameters=  myParser.getURLpath();
+        if(urlParameters.length <2){
+            /// todo error
+            return;
+        }
+        String userType= urlParameters[1]; /// register/user , register/admin , register/storeowner
+        switch (userType){
+            case "user":
+                this.registerCustomer(exchange);
+                break;
+            case "admin":
+                this.registerAdmin(exchange);
+                break;
+            case "storeowner":
+                this.registerStoreOwner(exchange);
+                break;
+            default:
+                ///todo
+                break;
+        }
+    }
+
+    public void registerCustomer(HttpExchange exchange){
+
+    }
     public void registerStoreOwner(HttpExchange exchange){}
     public void registerAdmin(HttpExchange exchange){}
 
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
-
-    }
 }

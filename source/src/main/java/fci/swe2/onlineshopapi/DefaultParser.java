23d0bdfed53abcc92203833464491f16c8cd7076
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-29 16:15:50 +0200

    Add constructor for DefaultParser

--- source/src/main/java/fci/swe2/onlineshopapi/DefaultParser.java
+++ source/src/main/java/fci/swe2/onlineshopapi/DefaultParser.java
@@ -5,8 +5,9 @@ import com.sun.net.httpserver.HttpExchange;
 public class DefaultParser implements HTTPExchangeParser {
     private HttpExchange exchange;
 
+    DefaultParser(HttpExchange exchange){}
+
     public String getURLpath(){}
     public String getParameter(String method, String key){}
     public String parseBody(){}
-    public String parser(HttpExchange exchange){}
 }

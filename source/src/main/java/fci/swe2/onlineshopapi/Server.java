8fda8c5a0a6ceae8a9720d4108562521964f5991
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-29 16:40:53 +0200

    Add main methos to Server in order to run program

--- source/src/main/java/fci/swe2/onlineshopapi/Server.java
+++ source/src/main/java/fci/swe2/onlineshopapi/Server.java
@@ -3,4 +3,10 @@ package fci.swe2.onlineshopapi;
 public class Server {
     public void initserver(){}
     public void startserver(){}
+
+    public static void main(String[] args){
+        Server server = new Server();
+        server.initserver();
+        server.startserver();
+    }
 }

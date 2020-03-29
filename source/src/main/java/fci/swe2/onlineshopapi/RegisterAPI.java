ed98536ac8b27b5d56a4f1af43ceee136f59609f
ahmeddrawy <ahmedhanfy1111@gmail.com>
2020-03-29 17:31:04 +0200

    make register api implements httpHandler

--- source/src/main/java/fci/swe2/onlineshopapi/RegisterAPI.java
+++ source/src/main/java/fci/swe2/onlineshopapi/RegisterAPI.java
@@ -1,9 +1,17 @@
 package fci.swe2.onlineshopapi;
 
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
-public class RegisterAPI {
+import java.io.IOException;
+
+public class RegisterAPI implements HttpHandler {
     public void registerCustomer(HttpExchange exchange){}
     public void registerStoreOwner(HttpExchange exchange){}
     public void registerAdmin(HttpExchange exchange){}
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
 }

81dcea4e986a0abd6ee5efd28ba7119fc97a5cb9
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-29 17:16:29 +0200

    Rename UserAPI to RegisterAPI

--- source/src/main/java/fci/swe2/onlineshopapi/UserAPI.java
+++
@@ -1,9 +0,0 @@
-package fci.swe2.onlineshopapi;
-
-import com.sun.net.httpserver.HttpExchange;
-
-public class UserAPI {
-    public void registerCustomer(HttpExchange exchange){}
-    public void registerStoreOwner(HttpExchange exchange){}
-    public void registerAdmin(HttpExchange exchange){}
-}

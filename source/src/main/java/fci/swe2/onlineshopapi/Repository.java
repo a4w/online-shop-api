725a02d76e4d052fdfd1d7636e2d22c6b3adf9a7
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-30 03:15:14 +0200

    Change return type in Repository interface to bool

--- source/src/main/java/fci/swe2/onlineshopapi/Repository.java
+++ source/src/main/java/fci/swe2/onlineshopapi/Repository.java
@@ -3,7 +3,7 @@ package fci.swe2.onlineshopapi;
 import fci.swe2.onlineshopapi.exceptions.*;
 
 public interface Repository<T> {
-    public long exists(long id);
+    public boolean exists(long id);
     public long retrieve(long id) throws ObjectNotFoundException;
     public T[] retrieveAll();
     public void store(T obj) throws ValidationException;

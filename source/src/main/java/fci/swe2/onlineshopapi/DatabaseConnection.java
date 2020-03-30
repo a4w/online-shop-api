4d68169140ad85318eb20c70b79b544725506bcb
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-30 02:45:54 +0200

    Add singelton database connection class

---
+++ source/src/main/java/fci/swe2/onlineshopapi/DatabaseConnection.java
@@ -0,0 +1,39 @@
+package fci.swe2.onlineshopapi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class DatabaseConnection{
+    final String DB_HOST = "localhost";
+    final String DB_NAME = "online-shop-api";
+    final String DB_USER = "root";
+    final String DB_PASS = "12341234";
+
+    private static DatabaseConnection instance = null;
+
+    private Connection connection;
+
+    private DatabaseConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://" + DB_HOST + "/" + DB_NAME + "?user=" + DB_USER + "&password=" + DB_PASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DatabaseConnection getInstace(){
+        if(instance == null)
+            instance = new DatabaseConnection();
+        return instance;
+    }
+
+    public static Connection getConnection(){
+        DatabaseConnection me = DatabaseConnection.getInstace();
+        return me.connection;
+    }
+
+};

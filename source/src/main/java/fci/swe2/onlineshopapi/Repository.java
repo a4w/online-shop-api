40e5e1cbdfef82fbcee44007448136dd5f088de6
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-30 03:30:12 +0200

    Fix return type in Repository interface

--- source/src/main/java/fci/swe2/onlineshopapi/Repository.java
+++ source/src/main/java/fci/swe2/onlineshopapi/Repository.java
@@ -4,7 +4,7 @@ import fci.swe2.onlineshopapi.exceptions.*;
 
 public interface Repository<T> {
     public boolean exists(long id);
-    public long retrieve(long id) throws ObjectNotFoundException;
+    public T retrieve(long id) throws ObjectNotFoundException;
     public T[] retrieveAll();
     public void store(T obj) throws ValidationException;
     public void update(T obj) throws ValidationException;

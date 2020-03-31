7904c5d554e17ffe03297fd369697f6586e81632
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-31 17:55:24 +0200

    Remove unused exceptions

--- source/src/main/java/fci/swe2/onlineshopapi/exceptions/PasswordTooShortException.java
+++
@@ -1,5 +0,0 @@
-package fci.swe2.onlineshopapi.exceptions;
-
-public class PasswordTooShortException extends ValidationException{
-    private static final long serialVersionUID = 7840844788649672221L;
-};
--- source/src/main/java/fci/swe2/onlineshopapi/exceptions/UsernameAlreadyExistsException.java
+++
@@ -1,5 +0,0 @@
-package fci.swe2.onlineshopapi.exceptions;
-
-public class UsernameAlreadyExistsException extends ValidationException{
-    private static final long serialVersionUID = 7840844788649672217L;
-};

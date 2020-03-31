e0448364d828102eb27a23ef9ec13be258a259d1
ahmeddrawy <ahmedhanfy1111@gmail.com>
2020-03-31 17:52:36 +0200

    add JsonStoreOwnerSerializer

---
+++ source/src/main/java/fci/swe2/onlineshopapi/dataWrappers/JsonStoreOwnerSerializer.java
@@ -0,0 +1,24 @@
+package fci.swe2.onlineshopapi.dataWrappers;
+import fci.swe2.onlineshopapi.StoreOwner;
+import org.json.JSONObject;
+
+public class JsonStoreOwnerSerializer implements Serializer<StoreOwner> {
+    @Override
+    public String serialize(StoreOwner storeOwner) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("email" , storeOwner.getEmail());
+        jsonObject.put("password" , storeOwner.getPassword());
+        jsonObject.put("username" , storeOwner.getUsername());
+        jsonObject.put("userID" , storeOwner.getUserID());
+        return jsonObject.toString();
+    }
+
+    @Override
+    public StoreOwner unserizlize(String str) {
+        JSONObject jsonObject = new JSONObject(str);
+        String email= jsonObject.get("email").toString();
+        String username= jsonObject.get("username").toString();
+        String password= jsonObject.get("password").toString();
+        return new StoreOwner(0, username , email,password) ;
+    }
+}

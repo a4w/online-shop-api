b9f404646cd376e556fc6abfb07ed3e65d8060c3
ahmeddrawy <ahmedhanfy1111@gmail.com>
2020-03-31 17:47:28 +0200

    removed customerWrapper and storeOwnerWrapper interfaces

--- source/src/main/java/fci/swe2/onlineshopapi/dataWrappers/CustomerWrapper.java
+++
@@ -1,8 +0,0 @@
-package fci.swe2.onlineshopapi.dataWrappers;
-
-import fci.swe2.onlineshopapi.Customer;
-
-public interface CustomerWrapper {
-    Customer getCustomer(String data);
-    String getData(Customer customer);
-}
--- source/src/main/java/fci/swe2/onlineshopapi/dataWrappers/StoreOwnerWrapper.java
+++
@@ -1,8 +0,0 @@
-package fci.swe2.onlineshopapi.dataWrappers;
-
-import fci.swe2.onlineshopapi.StoreOwner;
-
-public interface StoreOwnerWrapper {
-    StoreOwner getCustomer(String data);
-    String getData(StoreOwner customer);
-}

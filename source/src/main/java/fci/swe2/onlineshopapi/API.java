5fb833f3d925c9d1a2fd13ab7ea66a2d852da06a
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-31 19:50:58 +0200

    Change API super class interface

--- source/src/main/java/fci/swe2/onlineshopapi/API.java
+++ source/src/main/java/fci/swe2/onlineshopapi/API.java
@@ -7,22 +7,31 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public abstract class API implements HttpHandler {
-    HTTPExchangeParser myParser = null;
-    HttpExchange myExchange = null;
+    HTTPExchangeParser parser = null;
+    HttpExchange exchange = null;
+    String requestBody = null;
+
     @Override
-    abstract public void handle(HttpExchange exchange) throws IOException ;
+    abstract public void handle(HttpExchange exchange);
+
     void setParserExchange(HttpExchange exchange){
-        myParser= new DefaultParser(exchange);
-        myExchange = exchange;
+        this.parser = new DefaultParser(exchange);
+        this.exchange = exchange;
+        try{
+            this.requestBody = parser.parseBody();
+        }catch(Exception e){
+            // TODO: Error occured
+        }
     }
-    private void sendResponse(String str){
+
+    protected void sendResponse(String str){
         try {
-            myExchange.sendResponseHeaders(200, str.length());
-            OutputStream os = myExchange.getResponseBody();
+            exchange.sendResponseHeaders(200, str.length());
+            OutputStream os = exchange.getResponseBody();
             os.write(str.getBytes());
             os.close();
         } catch (IOException e) {
-            /// todo
+            /// TODO
             e.printStackTrace();
         }
     }

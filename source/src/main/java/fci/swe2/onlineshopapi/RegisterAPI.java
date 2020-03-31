d2a6b85f7c6253343ff24e9ca8d4b8138d720e43
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-31 19:51:52 +0200

    Change RegisterAPI to be a sub-class of API

--- source/src/main/java/fci/swe2/onlineshopapi/RegisterAPI.java
+++ source/src/main/java/fci/swe2/onlineshopapi/RegisterAPI.java
@@ -14,88 +14,60 @@ import fci.swe2.onlineshopapi.dataWrappers.SerializerFactory;
 import fci.swe2.onlineshopapi.dataWrappers.SerializerFactory.Type;
 import fci.swe2.onlineshopapi.dataWrappers.Serializer;
 
-public class RegisterAPI implements HttpHandler {
-    HTTPExchangeParser myParser = null;
-    JSONObject getRequestJson = null ;
+public class RegisterAPI extends API {
+
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        myParser = new DefaultParser(exchange);
-        String []urlParameters=  myParser.getURLpath();
-        try{
-            getRequestJson = myParser.parseBody();
-        }catch(Exception e){
-            JSONObject jobj = new JSONObject();
-            jobj.put("error", true);
-            jobj.put("error_type", "Malformed request");
-            sendResponse(exchange, jobj);
-        }
+    public void handle(HttpExchange exchange){
+        this.setParserExchange(exchange);
+
+        String[] urlParameters = parser.getURLpath();
+
         if(urlParameters.length <2){
-            /// todo error
+            // TODO: MalformedRequestException
             return;
         }
+
         String userType= urlParameters[1]; /// register/user , register/admin , register/storeowner
         switch (userType){
             case "customer":
-                this.registerCustomer(exchange);
+                this.registerCustomer();
                 break;
             case "admin":
-                //this.registerAdmin(exchange);
+                this.registerAdmin();
                 break;
             case "storeowner":
-                //this.registerStoreOwner(exchange);
+                this.registerStoreOwner();
                 break;
             default:
-                ///todo
+                // TODO: MalformedRequestException
                 break;
         }
     }
 
-    private void registerCustomer(HttpExchange exchange){
-        Serializer<Customer> customerWrapper = SerializerFactory.getSerializer(Customer.class, Type.JSON); // code to implementation we need a solution
-        Customer customer = customerWrapper.unserialize(getRequestJson.toString());
-        registerAccount(exchange,customer);
+    private void registerCustomer(){
+        Serializer<Customer> customerWrapper = SerializerFactory.getSerializer(Customer.class, Type.JSON);
+        Customer customer = customerWrapper.unserialize(this.requestBody);
+        registerAccount(customer);
     }
 
-    private void registerStoreOwner(HttpExchange exchange){
+    private void registerStoreOwner(){
         Serializer<StoreOwner> storeOwnerSerializer = SerializerFactory.getSerializer(StoreOwner.class , Type.JSON);
-        StoreOwner storeOwner =  storeOwnerSerializer.unserialize(getRequestJson.toString());
-        registerAccount(exchange , storeOwner);
-    }
-    private void registerAdmin(HttpExchange exchange){
-        Serializer<Admin> adminSerializer = SerializerFactory.getSerializer(Admin.class , Type.JSON);
-        Admin admin = adminSerializer.unserialize(getRequestJson.toString());
-        registerAccount(exchange,admin);
+        StoreOwner storeOwner =  storeOwnerSerializer.unserialize(this.requestBody);
+        registerAccount(storeOwner);
     }
 
-    private void registerAccount(HttpExchange exchange,Account account){
+    private void registerAdmin(){
+        Serializer<Admin> adminSerializer = SerializerFactory.getSerializer(Admin.class , Type.JSON);
+        Admin admin = adminSerializer.unserialize(this.requestBody);
+        registerAccount(admin);
+    }
+
+    private void registerAccount(Account account){
         try {
             account.register();
         }catch (ValidationException e){
             Serializer<UserFriendlyError> serializer = SerializerFactory.getSerializer(UserFriendlyError.class, Type.JSON);
-            sendResponse(exchange, serializer.serialize(e));
-        }
-    }
-    private void sendResponse(HttpExchange exchange,JSONObject jsonObject){
-        String response = jsonObject.toString();
-        try {
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        } catch (IOException e) {
-            /// todo
-            e.printStackTrace();
-        }
-    }
-    private void sendResponse(HttpExchange exchange,String str){
-        try {
-            exchange.sendResponseHeaders(200, str.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(str.getBytes());
-            os.close();
-        } catch (IOException e) {
-            /// todo
-            e.printStackTrace();
+            sendResponse(serializer.serialize(e));
         }
     }
 }

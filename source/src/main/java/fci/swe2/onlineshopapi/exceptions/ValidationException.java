c969d07badc61c5c72216d5b604b0807ef455220
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-31 17:42:04 +0200

    Changed Validation Exception

--- source/src/main/java/fci/swe2/onlineshopapi/exceptions/ValidationException.java
+++ source/src/main/java/fci/swe2/onlineshopapi/exceptions/ValidationException.java
@@ -1,5 +1,16 @@
 package fci.swe2.onlineshopapi.exceptions;
 
-public class ValidationException extends Exception{
+public class ValidationException extends Exception implements UserFriendlyError{
     private static final long serialVersionUID = 7840844788649672208L;
+
+    private String userFriendlyMessage;
+
+    ValidationException(String userFriendlyMessage){
+        this.userFriendlyMessage = userFriendlyMessage;
+    }
+
+    public String getUserFriendlyError(){
+        return this.userFriendlyMessage;
+    }
+
 };

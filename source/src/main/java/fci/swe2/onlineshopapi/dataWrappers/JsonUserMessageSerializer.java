9638933c06eebf5b32aa1e25921a2be65054eeb8
ahmeddrawy <ahmedhanfy1111@gmail.com>
2020-03-31 18:51:24 +0200

    add JsonUserMessageSerializer

---
+++ source/src/main/java/fci/swe2/onlineshopapi/dataWrappers/JsonUserMessageSerializer.java
@@ -0,0 +1,20 @@
+package fci.swe2.onlineshopapi.dataWrappers;
+
+import fci.swe2.onlineshopapi.exceptions.UserFriendlyError;
+import org.json.JSONObject;
+
+public class JsonUserMessageSerializer implements Serializer<UserFriendlyError> {
+    @Override
+    public String serialize(UserFriendlyError obj) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("error" , true);
+        jsonObject.put("error_type" , obj.getUserFriendlyError());
+        return  jsonObject.toString();
+    }
+
+    @Override
+    public UserFriendlyError unserialize(String str) {
+        /// todo we don't need this
+        return null;
+    }
+}

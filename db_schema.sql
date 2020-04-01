98c15760e3e8a200abe897e05122908524c78f58
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-01 18:50:37 +0200

    Add StoreOwner and Admin tables in db schema

--- db_schema.sql
+++ db_schema.sql
@@ -11,3 +11,21 @@ CREATE TABLE `Customer` (
     `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `StoreOwner` (
+    `id` INT AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `password` CHAR(60) NOT NULL,
+    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Admin` (
+    `id` INT AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `password` CHAR(60) NOT NULL,
+    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);

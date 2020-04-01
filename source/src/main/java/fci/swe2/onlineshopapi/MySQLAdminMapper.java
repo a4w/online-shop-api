23c63dd0798627ca323fa65adb79a61947ac4aed
okhaled9 <36415286+okhaled9@users.noreply.github.com>
2020-04-01 19:39:07 +0200

    Add files via upload

--- source/src/main/java/fci/swe2/onlineshopapi/MySQLAdminMapper.java
+++ source/src/main/java/fci/swe2/onlineshopapi/MySQLAdminMapper.java
@@ -1,5 +1,12 @@
 package fci.swe2.onlineshopapi;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+
 public class MySQLAdminMapper implements Repository<Admin> {
 
     private static MySQLAdminMapper instance = null;
@@ -23,10 +30,47 @@ public class MySQLAdminMapper implements Repository<Admin> {
     }
 
     public Admin[] retrieveAll(){
-        return null;
+        System.out.println("Retrieving all Admins");
+        PreparedStatement stmt = dbConnection.prepareStatement("Select * from Admin");
+        ResultSet result = stmt.executeQuery();
+        ArrayList admins = new ArrayList();
+        int i=0;
+        while(result.next){
+            long ID = result.getLong(1);
+            String username = result.getString(2);
+            String email = result.getString(3);
+            String password = result.getString(4);
+            Admin ob = new Admin(ID, username, email, password);
+            admins.add(ob);
+        }
+        return admins;
     }
 
-    public void store(Admin obj){}
+    public void store(Admin obj){
+        try {
+            System.out.println("Storing a user");
+            PreparedStatement stmt = dbConnection.prepareStatement("INSERT INTO `Admin` (`email`, `username`, `password`) VALUES (?, ?, ?)");
+            bindAdmin(obj, stmt);
+            stmt.executeUpdate();
+        } catch (SQLIntegrityConstraintViolationException e) {
+            e.printStackTrace();
+            final String message = e.getMessage();
+            // TODO: Maybe find a better way to handle this (this may cause wrong error messages if the value contains a key word)
+            if(message.contains("email")){
+                throw new ValidationException("Email already exists");
+            }else if(message.contains("username")){
+                throw new ValidationException("Username already exists");
+            }
+        } catch (SQLException e){
+            throw new ValidationException("An error occured please try again later");
+        }
+    }
     public void update(Admin obj){}
     public void delete(Admin obj){}
+
+    private void bindAdmin(Admin admin, PreparedStatement stmt) throws SQLException{
+        stmt.setString(1, admin.getEmail());
+        stmt.setString(2, admin.getUsername());
+        stmt.setString(3, admin.getPassword());
+    }
 }

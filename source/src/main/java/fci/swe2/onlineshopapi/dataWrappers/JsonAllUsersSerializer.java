b52bf1fc7b15a7a4511474796ae0f51a15eb834f
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-01 20:18:31 +0200

    Add the type in the all user serializer

--- source/src/main/java/fci/swe2/onlineshopapi/dataWrappers/JsonAllUsersSerializer.java
+++ source/src/main/java/fci/swe2/onlineshopapi/dataWrappers/JsonAllUsersSerializer.java
@@ -15,16 +15,19 @@ public class JsonAllUsersSerializer implements Serializer<AllUsersWrapper> {
         Serializer<Customer> customerSerializer = SerializerFactory.getSerializer(Customer.class, Type.JSON);
         for(Customer customer : obj.customers){
             JSONObject cobj = new JSONObject(customerSerializer.serialize(customer));
+            cobj.put("type", "Customer");
             arr.put(cobj);
         }
         Serializer<StoreOwner> storeOwnerSerializer = SerializerFactory.getSerializer(StoreOwner.class, Type.JSON);
         for(StoreOwner storeOwner : obj.storeOwners){
             JSONObject sobj = new JSONObject(storeOwnerSerializer.serialize(storeOwner));
+            sobj.put("type", "Store Owner");
             arr.put(sobj);
         }
         Serializer<Admin> adminSerializer = SerializerFactory.getSerializer(Admin.class, Type.JSON);
         for(Admin admin : obj.admins){
             JSONObject aobj = new JSONObject(adminSerializer.serialize(admin));
+            aobj.put("type", "Admin");
             arr.put(aobj);
         }
         jsonObject.put("users", arr);

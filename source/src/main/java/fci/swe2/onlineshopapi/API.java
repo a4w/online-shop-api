89a0d71b1fa86fa344a82478a4bfc725990395b5
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-01 17:34:01 +0200

    Better handling of conent type errors

--- source/src/main/java/fci/swe2/onlineshopapi/API.java
+++ source/src/main/java/fci/swe2/onlineshopapi/API.java
@@ -8,6 +8,7 @@ import fci.swe2.onlineshopapi.dataWrappers.Serializer;
 import fci.swe2.onlineshopapi.dataWrappers.SerializerFactory;
 import fci.swe2.onlineshopapi.dataWrappers.SerializerFactory.Type;
 import fci.swe2.onlineshopapi.exceptions.UserFriendlyError;
+import fci.swe2.onlineshopapi.exceptions.ValidationException;
 import fci.swe2.onlineshopapi.exceptions.MalformedRequestException;
 
 import java.io.IOException;
@@ -22,23 +23,28 @@ public abstract class API implements HttpHandler {
     @Override
     abstract public void handle(HttpExchange exchange);
 
-    void setParserExchange(HttpExchange exchange){
+    void setParserExchange(HttpExchange exchange) throws Exception{
         this.exchange = exchange;
         this.parser = new DefaultParser(exchange);
         try{
             this.requestBody = parser.parseBody();
-            Headers headers = exchange.getRequestHeaders();
-            String contentType = headers.getFirst("Content-Type");
-            switch(contentType){
-                case "application/json":
-                default:
-                    this.responseType = Type.JSON;
-                    break;
-                // Add other return types here if required
-            }
-
         }catch(Exception e){
+            System.out.println("Malformed ");
             sendMalformedRequestError();
+            throw e;
+        }
+        Headers headers = exchange.getRequestHeaders();
+        String contentType = headers.getFirst("Content-Type");
+        switch(contentType){
+            case "application/json":
+                this.responseType = Type.JSON;
+                break;
+            // Add other return types here if required
+
+            default:
+                System.out.println("Content ");
+                sendWrongContentTypeError();
+                throw new Exception(); // To be catched in caller
         }
     }
 
@@ -53,14 +59,31 @@ public abstract class API implements HttpHandler {
         }
     }
 
+    protected void sendResponse(String str, int statusCode){
+        try {
+            exchange.sendResponseHeaders(statusCode, str.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(str.getBytes());
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendWrongContentTypeError(){
+        UserFriendlyError error = new ValidationException("The requested content type is not supported");
+        Serializer<UserFriendlyError> serializer = SerializerFactory.getSerializer(UserFriendlyError.class, this.responseType);
+        sendResponse(serializer.serialize(error), 415);
+    }
+
     protected void sendMalformedRequestError(){
         UserFriendlyError error = new MalformedRequestException("Malformed request");
-        Serializer<UserFriendlyError> serializer = SerializerFactory.getSerializer(UserFriendlyError.class, Type.JSON);
-        sendResponse(serializer.serialize(error));
+        Serializer<UserFriendlyError> serializer = SerializerFactory.getSerializer(UserFriendlyError.class, this.responseType);
+        sendResponse(serializer.serialize(error), 400);
     }
 
     protected void sendOkRequest(){
-        Serializer<String> serializer = SerializerFactory.getSerializer(String.class, Type.JSON);
+        Serializer<String> serializer = SerializerFactory.getSerializer(String.class, this.responseType);
         sendResponse(serializer.serialize("Request completed successfully"));
     }
 

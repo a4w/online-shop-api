4cd4d5706237267c1947c4fef446942a7c127ede
abdelrahmanIEl-Batal <abdelrahman.elbatal19@gmail.com>
2020-04-15 21:48:37 +0200

    Added generateToken then update token for signed in user

--- source/src/main/java/fci/swe2/onlineshopapi/DefaultLogin.java
+++ source/src/main/java/fci/swe2/onlineshopapi/DefaultLogin.java
@@ -9,12 +9,25 @@ public class DefaultLogin implements LoginBehaviour{
         if(account!=null){   // assumed findByUsername returns null if not found
             Account temp = (Account) obj;
             if(account.getPassword().equals(temp.getPassword())){
-                // generate token
+                String token = generateToken(10);
+                repo.updateToken(obj,token);
             }
         }
         // handle more details
         else{
 
         }
+    }
+
+    String generateToken(int n){
+        final String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+        StringBuilder string = new StringBuilder();
+        while (n> 0 )
+        {
+            int pos = (int)(Math.random()*alpha.length());
+            string.append(alpha.charAt(pos));
+            n--;
+        }
+        return string.toString();
     }
 }

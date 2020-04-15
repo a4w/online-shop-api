dc6d6572c651ea70c4bc229c5e495944a6dfec04
abdelrahmanIEl-Batal <abdelrahman.elbatal19@gmail.com>
2020-04-15 21:47:00 +0200

    Added updateToken function

--- source/src/main/java/fci/swe2/onlineshopapi/AccountRepository.java
+++ source/src/main/java/fci/swe2/onlineshopapi/AccountRepository.java
@@ -4,4 +4,5 @@ public interface AccountRepository<T> {
     public T findByUsername(T obj);
     public T findByEmail(T obj);
     public T findByToken(T obj);
+    public void updateToken(T obj, String token);
 }

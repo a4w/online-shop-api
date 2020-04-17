bd1bca216a9d6696b9cd57c96afacd77c8c8f496
abdelrahmanIEl-Batal <abdelrahman.elbatal19@gmail.com>
2020-04-17 23:25:24 +0200

    create login for each user type

--- source/src/main/java/fci/swe2/onlineshopapi/LoginAPI.java
+++ source/src/main/java/fci/swe2/onlineshopapi/LoginAPI.java
@@ -1,6 +1,9 @@
 package fci.swe2.onlineshopapi;
 
 import com.sun.net.httpserver.HttpExchange;
+import fci.swe2.onlineshopapi.dataWrappers.LoginRequestWrapper;
+import fci.swe2.onlineshopapi.dataWrappers.Serializer;
+import fci.swe2.onlineshopapi.dataWrappers.SerializerFactory;
 
 public class LoginAPI extends API {
     @Override
@@ -22,13 +25,13 @@ public class LoginAPI extends API {
         String userType= urlParameters[1]; /// login/user , login/admin , login/storeowner
         switch (userType){
             case "customer":
-                this.loginAccount();
+                this.loginCustomer();
                 break;
             case "admin":
-                this.loginAccount();
+                this.loginAdmin();
                 break;
             case "storeowner":
-                this.loginAccount();
+                this.loginStoreOwner();
                 break;
             default:
                 sendMalformedRequestError();
@@ -36,11 +39,40 @@ public class LoginAPI extends API {
         }
     }
 
+    private void loginCustomer(){
+        Serializer<LoginRequestWrapper> serializer = SerializerFactory.getSerializer(LoginRequestWrapper.class,this.responseType);
+        LoginRequestWrapper wrapper = serializer.unserialize(this.requestBody);
+        Customer customer = new Customer();
+        customer.setUsername(wrapper.getUsername());
+        customer.setPassword(wrapper.getPassword());
+        loginAccount(customer);
+    }
+
+    private void loginAdmin(){
+        Serializer<LoginRequestWrapper> serializer = SerializerFactory.getSerializer(LoginRequestWrapper.class,this.responseType);
+        LoginRequestWrapper wrapper = serializer.unserialize(this.requestBody);
+        Admin admin = new Admin();
+        admin.setUsername(wrapper.getUsername());
+        admin.setPassword(wrapper.getPassword());
+        loginAccount(admin);
+    }
+
+    private void loginStoreOwner(){
+        Serializer<LoginRequestWrapper> serializer = SerializerFactory.getSerializer(LoginRequestWrapper.class,this.responseType);
+        LoginRequestWrapper wrapper = serializer.unserialize(this.requestBody);
+        StoreOwner storeOwner = new StoreOwner();
+        storeOwner.setUsername(wrapper.getUsername());
+        storeOwner.setPassword(wrapper.getPassword());
+        loginAccount(storeOwner);
+    }
+    /*
     private void loginAccount(){
 
         Admin admin = new Admin(1,"admin","email@gmail.com","123456789");
         loginAccount((Account) admin);
     }
+    */
+
     private void loginAccount(Account account){
         sendResponse(account.login());
     }

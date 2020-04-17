74502c04d25aa24a28104edb72a3fcfb9e2ff547
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-17 23:23:06 +0200

    Change account repository to use strings and throw exceptions

--- source/src/main/java/fci/swe2/onlineshopapi/AccountRepository.java
+++ source/src/main/java/fci/swe2/onlineshopapi/AccountRepository.java
@@ -1,6 +1,8 @@
 package fci.swe2.onlineshopapi;
 
+import fci.swe2.onlineshopapi.exceptions.ObjectNotFoundException;
+
 public interface AccountRepository<T> {
-    public T findByUsername(T obj);
-    public T findByEmail(T obj);
+    public T findByUsername(String obj) throws ObjectNotFoundException;
+    public T findByEmail(String obj) throws ObjectNotFoundException;
 }
